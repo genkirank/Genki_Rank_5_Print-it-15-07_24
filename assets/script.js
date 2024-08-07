bca6081a65bd338dbc1015fe23a6c6dea8d76e6d
Raphal Luis <raphluis11@gmail.com>
2024-08-07 22:45:46 +0200

    /

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,57 @@
-const slides = [
-  {
-    image: "slide1.jpg",
-    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
-  },
-  {
-    image: "slide2.jpg",
-    tagLine: "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
-  },
-  {
-    image: "slide3.jpg",
-    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
-  },
-  {
-    image: "slide4.png",
-    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
-  },
-];
-
-// Fonction pour gérer le clic sur la flèche gauche
-function handleLeftArrowClick() {
-  console.log("Flèche gauche cliquée !");
-  // Ici, vous pouvez ajouter la logique pour changer d'image
-}
-
-// Fonction pour gérer le clic sur la flèche droite
-function handleRightArrowClick() {
-  console.log("Flèche droite cliquée !");
-  // Ici, vous pouvez ajouter la logique pour changer d'image
-}
-
-// Attendre que le document soit chargé
 document.addEventListener("DOMContentLoaded", function () {
-  // Sélectionner les flèches
-  const leftArrow = document.querySelector(".arrow_left");
-  const rightArrow = document.querySelector(".arrow_right");
+  const slides = [
+      {
+          image: "slide1.jpg",
+          tagLine: "Impressions tous formats <span>en boutique et en ligne</span>"
+      },
+      {
+          image: "slide2.jpg",
+          tagLine: "Tirages haute définition grand format <span>pour vos bureaux et événements</span>"
+      },
+      {
+          image: "slide3.jpg",
+          tagLine: "Impressions sur <span>tous types de supports</span>"
+      },
+      {
+          image: "slide4.png",
+          tagLine: "Bannières, affiches, et bien plus <span>à des prix compétitifs</span>"
+      }
+  ];
 
-  // Ajouter les event listeners
-  leftArrow.addEventListener("click", handleLeftArrowClick);
-  rightArrow.addEventListener("click", handleRightArrowClick);
+  let currentSlide = 0;
+
+  const bannerImg = document.querySelector(".banner-img");
+  const bannerTagLine = document.querySelector("#banner p");
+  const dots = document.querySelectorAll(".dot");
+
+  function updateBanner() {
+      bannerImg.src = `./assets/images/slideshow/${slides[currentSlide].image}`;
+      bannerTagLine.innerHTML = slides[currentSlide].tagLine;
+      dots.forEach((dot, index) => {
+          dot.classList.toggle("dot_selected", index === currentSlide);
+      });
+      
+  }
+
+ 
+
+  document.querySelector(".arrow_left").addEventListener("click", () => {
+      currentSlide = (currentSlide > 0) ? currentSlide - 1 : slides.length - 1;
+      updateBanner();
+  });
+
+  document.querySelector(".arrow_right").addEventListener("click", () => {
+      currentSlide = (currentSlide < slides.length - 1) ? currentSlide + 1 : 0;
+      updateBanner();
+  });
+
+  dots.forEach((dot, index) => {
+      dot.addEventListener("click", () => {
+          currentSlide = index;
+          updateBanner();
+      });
+  });
+
+  updateBanner(); 
 });
+
